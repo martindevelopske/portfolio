483982fc7d0dcfeaf7956abf93b9b86269771a78
martin ndungu <martindevelopske@gmail.com>
2022-05-16 08:42:57 +0300

    added styles

---
+++ site/src/styles/home.styled.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+export const StyledHomeContainer=styled.div`
+width:100%;
+
+`
---
+++ site/src/styles/link.styled.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+const LinkStyled=styled.Link`
+color: red;
+`
+export default LinkStyled;
